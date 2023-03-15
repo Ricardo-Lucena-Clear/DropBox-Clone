af1a74aa894dc83fc601a6c2ff13dc7952b0cf0a
Ricardo Lucena <ricardo.lucena.clear@gmail.com>
2023-03-15 09:13:13 -0300

    Renaming file

--- public/src/controller/DropBoxController.js
+++ public/src/controller/DropBoxController.js
@@ -30,13 +30,25 @@ class DropBoxController {
 
         firebase.initializeApp(firebaseConfig);
     }
-    getSlection(){
+    getSelection(){
 
       return this.listFilesEl.querySelectorAll('.selected');
     }
     initEvents() {
+
+      this.btnRename.addEventListener('click', e=>{
+        let li = this.getSelection()[0];
+        let file = JSON.parse(li.dataset.file);
+        let name = prompt("Renomear o arquivo", file.name);
+        if (name){
+          file.name = name;
+          this.getFirebaseRef().child(li.dataset.key).set(file);
+        }
+      })
+
+
       this.listFilesEl.addEventListener('selectionchange', e=>{
-        switch (this.getSlection().length){
+        switch (this.getSelection().length){
           case 0:
           this.btnDelete.style.display = 'none';
           this.btnRename.style.display = 'none';
@@ -315,7 +327,8 @@ class DropBoxController {
 
         let li = document.createElement('li')
     
-        li.dataset.key = key
+        li.dataset.key = key;
+        li.dataset.file = JSON.stringify(file);
     
         li.innerHTML = `
           ${this.getFileIconView(file)}
