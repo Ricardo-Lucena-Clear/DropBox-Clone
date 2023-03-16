25be7a09023da6fc009c91fc608f0ba59ee8bc8b
Ricardo Lucena <ricardo.lucena.clear@gmail.com>
2023-03-16 09:02:03 -0300

    Creating route to open file

--- public/src/controller/DropBoxController.js
+++ public/src/controller/DropBoxController.js
@@ -412,51 +412,72 @@ class DropBoxController {
         this.lastFolder = this.currentFolder.join('/');
       this.getFirebaseRef().on("value", (snapshot) => {
       this.listFilesEl.innerHTML = "";
-      snapshot.forEach(item => {
-        let data = item.val();
-        if (data.type || data.mimetype) this.listFilesEl.appendChild(this.getFileIconHtml(data, item.key));
+      snapshot.forEach((snapshotItem) => {
+        let key = snapshotItem.key;
+        let data = snapshotItem.val();
+        if (data.type) {
+          this.listFilesEl.appendChild(this.getFileView(data, key));
+        }
+      });
     });
-});
-}
+  }
       openFolder(){
         if (this.lastFolder) this.getFirebaseRef(this.lastFolder).off('value');
         this.renderNav();
         this.readFiles();
       }
-      renderNav() {
-        let nav = document.createElement('nav')
+      renderNav(){
+
+        let nav = document.createElement('nav');
         let path = [];
+
         for (let i = 0; i < this.currentFolder.length; i++) {
-          let name= this.currentFolder[i];
-          let span = document.createElement('span');
-          path.push(name);
-          if ((i + 1) === this.currentFolder.length) {
-            span.innerHTML = name;   
-          } else {
-            span.className = 'breadcrumb-segment__wrapper'
-            span.innerHTML = `
-              <span class="ue-effect-container uee-BreadCrumbSegment-link-0">
-                <a href="#" data-path="${path.join('/')}" class="breadcrumb-segment">${folderName}</a>
-              </span>
-              <svg width="24" height="24" viewBox="0 0 24 24" class="mc-icon-template-stateless" style="top: 4px; position: relative;">
-                <title>arrow-right</title>
-                <path d="M10.414 7.05l4.95 4.95-4.95 4.95L9 15.534 12.536 12 9 8.464z" fill="#637282" fill-rule="evenodd"></path>
-              </svg>
-            `
-          }
-          nav.appendChild(span);
+
+            let folderName = this.currentFolder[i];
+            let span = document.createElement('span');
+
+            path.push(folderName);
+
+            if ((i + 1) === this.currentFolder.length) {
+
+                span.innerHTML = folderName;
+
+            } else {
+
+                span.className = 'breadcrumb-segment__wrapper';
+                span.innerHTML = `
+                    <span class="ue-effect-container uee-BreadCrumbSegment-link-0">
+                        <a href="#" data-path=${path.join('/')} class="breadcrumb-segment">${folderName}</a>
+                    </span>
+                    <svg width="24" height="24" viewBox="0 0 24 24" class="mc-icon-template-stateless" style="top: 4px; position: relative;">
+                        <title>arrow-right</title>
+                        <path d="M10.414 7.05l4.95 4.95-4.95 4.95L9 15.534 12.536 12 9 8.464z" fill="#637282"
+                            fill-rule="evenodd"></path>
+                    </svg>
+                `;
+
+            }
+
+            nav.appendChild(span);
+
         }
-        this.navEl.innerHTML = nav.innerHTML; 
-        
+
+        this.navEl = nav;
         this.navEl.querySelectorAll('a').forEach(a => {
-          a.addEventListener('click', e => {
-            e.preventDefault();
-            this.currentFolder = a.dataset.path.split('/');
-            this.openFolder()
-          })
-        })
-    
-      }
+
+            a.addEventListener('click', e => {
+
+                e.preventDefault();
+
+                this.currentFolder = a.dataset.path.split('/');
+
+                this.openFolder();
+
+            });
+
+        });
+
+    }
       
     initEventsLI(li){
       li.addEventListener('dblclick', e => {
@@ -467,7 +488,7 @@ class DropBoxController {
             this.openFolder()
             break;
           default:
-            window.open(file.path)
+            window.open('/file?path=' + file.path)
         }
       })
         li.addEventListener('click', e =>{
