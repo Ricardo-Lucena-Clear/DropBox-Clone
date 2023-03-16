74fc41a0dd9a66e037cde7039ab5c13c651a657b
Ricardo Lucena <ricardo.lucena.clear@gmail.com>
2023-03-16 09:50:41 -0300

    Deleting file from Firebase Storage

--- public/src/controller/DropBoxController.js
+++ public/src/controller/DropBoxController.js
@@ -42,13 +42,19 @@ class DropBoxController {
     this.getSelection().forEach((li) => {
       let file = JSON.parse(li.dataset.file);
       let key = li.dataset.key;
+      promises.push(new Promise((resolve, reject)=>{
+        let fileRef = firebase.storage().ref(this.currentFolder.join('/')).child(file.name);
+        fileRef.delete().then(()=>{
+          resolve({
+            fields:{
+              key
+            }
 
-      let formData = new FormData()
+          })
+        }).catch(err=>{
 
-      formData.append('path', file.path);
-      formData.append('key', key);
-
-      promises.push(this.ajax('/file', 'DELETE', formData));
+        })
+      }));
 
     });
     
